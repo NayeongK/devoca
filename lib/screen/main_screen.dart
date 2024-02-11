2a9fe43345927230c5504857d4d9b212e3500dd8
nayeong <nyk1118@naver.com>
2024-02-11 15:47:57 +0900

    feature: add navigation to study screen

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
 import "package:devoca/provider/model_vocabulary_provider.dart";
+import "package:devoca/screen/study_screen.dart";
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -34,32 +35,43 @@ class _MainListState extends State<MainList> {
                 itemCount: vocabularyProvider.vocabularyLists.length,
                 itemBuilder: (context, index) {
                   final vocabularyList = vocabularyProvider.vocabularyLists[index];
-                  return Container(
-                    margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          vocabularyList.title,
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.grey[800]
+                  return ListTile(
+                    title: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      StudyScreen(
+                                        vocabularyIndex: index,
+                                      )));
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                vocabularyList.title,
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.grey[800]
+                                ),
+                              ),
+                              const SizedBox(height: 13),
+                              Wrap(
+                                spacing: 8.0,
+                                children: vocabularyList.hashtags.map((tag) => Chip(
+                                  label: Text("# ${tag}",
+                                      style: const TextStyle(fontSize: 12)
+                                  ),
+                                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                )).toList(),
+                              ),
+                              const SizedBox(height: 45)
+                            ],
                           ),
-                        ),
-                        const SizedBox(height: 13),
-                        Wrap(
-                          spacing: 8.0,
-                          children: vocabularyList.hashtags.map((tag) => Chip(
-                            label: Text("# ${tag}",
-                                style: const TextStyle(fontSize: 12)
-                            ),
-                            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                          )).toList(),
-                        ),
-                        const SizedBox(height: 45)
-                      ],
-                    ),
+                        )),
                   );
                 },
               ),
