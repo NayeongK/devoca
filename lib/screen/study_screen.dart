5b7e9eb15b2dbd95fc8ecdb7d2f9bb3120ae825b
nayeong <nyk1118@naver.com>
2024-02-12 20:14:43 +0900

    feature: toggle between word or meaning on card tap

--- lib/screen/study_screen.dart
+++ lib/screen/study_screen.dart
@@ -12,11 +12,14 @@ class StudyScreen extends StatefulWidget {
 }
 
 class _StudyScreenState extends State<StudyScreen> {
+  bool showWord = true;
+
   @override
   Widget build(BuildContext context) {
     final vocabularyListProvider = Provider.of<VocabularyListProvider>(context);
     final vocabularyList = vocabularyListProvider.vocabularyLists[widget.vocabularyIndex];
-    final word = vocabularyList.words.isNotEmpty ? vocabularyList.words.first.word : "단어가 없습니다";
+    final wordItem = vocabularyList.words.isNotEmpty ? vocabularyList.words.first : null;
+    final textToShow = showWord ? wordItem?.word : wordItem?.meaning;
 
     return Scaffold(
       appBar: AppBar(
@@ -38,19 +41,25 @@ class _StudyScreenState extends State<StudyScreen> {
         ),
       ),
       body: Center(
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(35.0),
-            side: const BorderSide(color: Color(0xFFC6C7F7)),
-          ),
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              showWord = !showWord;
+            });
+          },
           child: Container(
             width: double.infinity,
             height: 400,
-            padding: const EdgeInsets.all(16.0),
             alignment: Alignment.center,
-            child: Text(word,
+            child: Text(
+              textToShow ?? "단어가 없습니다",
               style: const TextStyle(fontSize: 20),
               textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(35.0),
+              border: Border.all(color: const Color(0xFFC6C7F7)),
             ),
           ),
         ),
